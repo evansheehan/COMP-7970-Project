f8e429c92dbc9c8965816f4be238b68f502b7ddc
evansheehan <res0038@auburn.edu>
2017-11-14 12:04:23 -0600

    11/14/2017

--- src/DBScan.java
+++ src/DBScan.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 /**
  * Created by awubis on 11/6/17.
  */
 public class DBScan {
+
+   private class point {
+
+   }
+
+   public double calculateDistance() {
+      return 0;
+   }
 }
