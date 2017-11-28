2c051902e87062a17722e043b4a434bc65d66cb9
evansheehan <res0038@auburn.edu>
2017-11-28 12:23:00 -0600

    11/28/2017

--- src/DBScan.java
+++ src/DBScan.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Created by awubis on 11/6/17.
+ * Created by Evan on 11/6/2017.
  */
 
 public class DBScan {
@@ -13,9 +13,9 @@ public class DBScan {
    private ArrayList<Point> dataset;
 
    public DBScan(String fileName) throws FileNotFoundException {
+      dataset = new ArrayList<>();
       dataset = readInput(fileName);
    }
-
 
    class Point {
       String nodeID;
@@ -29,30 +29,43 @@ public class DBScan {
          this.nodeID = nodeID;
       }
 
-      private ArrayList<Point> getNeighbors(int nodeID) {
-         return null;
+      public Point(String nodeID, String neighbor) {
+         this.nodeID = nodeID;
+         if (neighbors == null) {
+            neighbors = new ArrayList<>();
+         }
+         neighbors.add(new Point(neighbor));
+      }
+
+      private void addNeighbor(String nodeID) {
+         Point point = new Point(nodeID);
+         neighbors.add(point);
       }
    }
 
    public ArrayList<Point> readInput(String fileName) throws FileNotFoundException {
-
       Scanner in = new Scanner(new FileInputStream(new File(fileName)));
+      String prevPoint = "";
 
       while (in.hasNext()) {
-         Point point = new Point(in.next());
-         if (!dataset.contains(point)) {
-            dataset.add(point);
-            in.next();
+         String line = in.nextLine();
+         if (!line.startsWith("#")) {
+            Point point = new Point(in.next(), in.next());
+            if (dataset.size() == 0) {
+               dataset.add(point);
+            } else if (prevPoint.compareTo(point.nodeID) == 0) {
+               point.addNeighbor(point.nodeID);
+               in.next();
+            }
+            prevPoint = point.nodeID;
          }
       }
-
       return dataset;
    }
 
    public ArrayList<ArrayList<Point>> dbScanAlgorithm(ArrayList<Point> allPoints, int radius, int minPts) {
       return null;
    }
-
    /*public double calculateDistance(Point point1, Point point2) {
       return 0;
    }*/
